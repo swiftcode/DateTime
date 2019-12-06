280908577700d640f336856613b4c0652302e417
mpc <swiftcode@users.noreply.github.com>
2019-12-06 14:44:30 -0600

    Update Contents.swift
    Add shortMonth and longMonth

--- Contents.swift
+++ Contents.swift
@@ -54,7 +54,7 @@ extension Date {
     var longDay: String {
         let calendar = Calendar.current
         let dayOfWeek = calendar.component(.weekday, from: self)
-        
+
         switch dayOfWeek {
             case 0: return "Sunday"
             case 1: return "Monday"
@@ -63,6 +63,48 @@ extension Date {
             case 4: return "Thursday"
             case 5: return "Friday"
             case 6: return "Saturday"
+            default: return ""
+        }
+    }
+
+    var shortMonth: String {
+        let calendar = Calendar.current
+        let month = calendar.component(.month, from: self)
+
+        switch month {
+            case 1: return "Jan"
+            case 2: return "Feb"
+            case 3: return "Mar"
+            case 4: return "Apr"
+            case 5: return "May"
+            case 6: return "Jun"
+            case 7: return "Jul"
+            case 8: return "Aug"
+            case 9: return "Sep"
+            case 10: return "Oct"
+            case 11: return "Nov"
+            case 12: return "Dec"
+            default: return ""
+        }
+    }
+
+    var longMonth: String {
+        let calendar = Calendar.current
+        let month = calendar.component(.month, from: self)
+
+        switch month {
+            case 1: return "January"
+            case 2: return "February"
+            case 3: return "March"
+            case 4: return "April"
+            case 5: return "May"
+            case 6: return "June"
+            case 7: return "July"
+            case 8: return "August"
+            case 9: return "September"
+            case 10: return "October"
+            case 11: return "November"
+            case 12: return "December"
             default: return ""
         }
     }
@@ -75,3 +117,5 @@ today.shortDate
 today.shortTime
 today.shortDay
 today.longDay
+today.shortMonth
+today.longMonth
